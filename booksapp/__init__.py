1b226c81cb53d59f9c40a7232ebf1546d44be1e1
kejbi <biegajski.kacper@gmail.com>
2019-07-30 22:29:17 +0200

    registration layout and features done, logic - to do

--- booksapp/__init__.py
+++ booksapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5bb9cf80d3f02f9603bf70d8d4fa1d7017bf1e09a94e5935'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 db = SQLAlchemy(app)
 
--- booksapp/routes.py
+++ booksapp/routes.py
@@ -1,7 +1,15 @@
 from booksapp import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash
+from booksapp.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/start')
 def start():
     return render_template('start.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}','success')
+    return render_template('register.html', form = form)
